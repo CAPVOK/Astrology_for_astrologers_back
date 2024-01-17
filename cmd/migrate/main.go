78ea26ca5e586b2efc1195af92c4f6af04bfb90c
CAPVOK <capvok@yandex.ru>
2024-01-17 15:41:07 +0300

    add mock

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -22,3 +22,31 @@ func main() {
 		panic("cant migrate db")
 	}
 }
+
+/* CREATE TABLE
+    constellations (
+        constellation_id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        start_date TIMESTAMP NOT NULL,
+        end_date TIMESTAMP NOT NULL,
+        moderator_id INTEGER,
+        user_id INTEGER NOT NULL,
+        constellation_status VARCHAR(11) CHECK (
+            constellation_status IN (
+                'created',
+                'inprogress',
+                'completed',
+                'deleted',
+                'canceled'
+            )
+        ),
+        creation_date TIMESTAMP NOT NULL,
+        formation_date TIMESTAMP,
+        confirmation_date TIMESTAMP,
+        FOREIGN KEY (moderator_id) REFERENCES "users"(user_id),
+        FOREIGN KEY (user_id) REFERENCES "users"(user_id)
+    );
+
+DROP TABLE constellations;
+
+DELETE FROM constellations; */
